7e72a85a3dff1a1f3fe8b30220311daf5e8f361f
Danny JeanLouis <dlouis3386@gmail.com>
2022-09-18 14:19:10 -0400

    add navbar logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
-const NavBar = () => {
-  return <div>NavBar</div>;
-};
+import Logo from "../logo.png";
+
+const NavBar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      postion: "sticky",
+      background: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={Logo} alt="logo" height={45} />
+    </Link>
+  </Stack>
+);
 
 export default NavBar;
